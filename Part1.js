772992268b334c61e82567ece31191e9cecd021f
Bruno Morgades <brunomorgades@hotmail.com>
2022-09-05 05:16:19 -0300

    Part 1 stylized and completed 100%

--- Part1.js
+++ Part1.js
@@ -11,15 +11,14 @@
         const element =(index)=>{
         const item=keys[index];
         const data=response.bpi[item];
-                    document.querySelector("h1").innerHTML=`${data.code} `;
+                    document.querySelector("h2").innerHTML=`${data.code} `;
                     document.querySelector("p").innerHTML=`${data.description} `;
                     document.querySelector("span").innerHTML= `${data.symbol} ${data.rate} `;
-
                     }
         const next=()=>{
-        index===0?index=length:index-=1;    
-        element(index);
-        }
+            index===0?index=length:index-=1;    
+            element(index);
+            }
         const previous=()=>{
             index===length?index=0:index+=1;    
             element(index);
